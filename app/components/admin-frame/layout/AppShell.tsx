8ee061f8ced1b6d8c1208ec8c02320270551bff4
yukiidayo <yukiha617@gmail.com>
2026-03-26 18:40:45 +0900

    Refactor admin shell navigation layout

--- app/components/admin-frame/layout/AppShell.tsx
+++ app/components/admin-frame/layout/AppShell.tsx
@@ -4,15 +4,19 @@ import { useNavState } from "~/hooks/useNavState";
 
 import { AppHeader } from "./AppHeader";
 import { AppSidebar } from "./AppSidebar";
+import { AppSidebarBrand } from "./AppSidebarBrand";
 
 export function AppShell() {
   const isOpen = useNavState((state) => state.isOpen);
 
   return (
     <div className="app-shell" data-nav-open={isOpen}>
-      <AppHeader />
-      <div className="app-shell__body">
+      <div className="app-shell__nav-column">
+        <AppSidebarBrand />
         <AppSidebar />
+      </div>
+      <div className="app-shell__content">
+        <AppHeader />
         <main className="app-shell__main">
           <Outlet />
         </main>
---
+++ app/components/admin-frame/layout/AppSidebarBrand.tsx
@@ -0,0 +1,10 @@
+export function AppSidebarBrand() {
+  return (
+    <div className="app-sidebar-brand">
+      <span className="app-sidebar-brand__mark">R</span>
+      <span className="app-sidebar-brand__text">
+        rec<em className="app-sidebar-brand__accent">time</em>
+      </span>
+    </div>
+  );
+}
